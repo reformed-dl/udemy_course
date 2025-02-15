a90dba70d4d9b51396a914edd6462cb51670bd54
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-15 16:01:55 -0500

    added comment and fixed println! macro

--- about_me/src/main.rs
+++ about_me/src/main.rs
@@ -1,6 +1,7 @@
 /*compile the code by navigating to the src folder and then from the terminal running rustc main.rs
 run the executable file by ./man
-you can compile and run the project by running cargo run */
+you can compile and run the project by running cargo run
+changing from println! to print! does not print separate lines, it is one continuous print line */
 
 fn main() {
     println!("My pseudonym is: reformed");
