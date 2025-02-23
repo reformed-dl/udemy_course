b6a6b33b7bec9990ba956b1462006c14e327834b
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-23 08:05:40 -0500

    Tuples Practice

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,11 @@
 fn main() {
-  //Arrays are fixed data type as well as fixed size(number of elements)
-  let numbers= [24, 56, 2, 89, 100];
-  println!("My numbers array: {:?}", numbers);//to print the full array, must be in debug {:?}
+  //tuples can be of multiple data types
+  let employee: (&str, i32, &str)  = ("Molly", 35, "Engineering");
+  println!("Employee Details {:?}", employee); //must use {:?} to print full tuple
+  //let name = employee.0;
+  //let age = employee.1;
+  //let department = employee.2;
+  let (name, age, department) = employee;//this is shorthand for the 3 commented out lines above
+  println!("Name: {}, Age: {}, Department: {}", name, age, department);
 
-  let apples= ["Granny Smith", "Red Delicious", "Honey Crisp"];
-  println!("My favorite apple type is: {}", apples[2]);//index position starts at 0
-  println!("The length of my apple array is {}", apples.len());//length method added
-
-  let seasons= ["Summer", "Spring", "Winter", "Fall"];
-  let first = seasons[0];
-  let second = seasons[1];
-  println!("The first season is {}, the second is {}", first, second);
-
-  let mut seasons= ["Summer", "Spring", "Winter", "Fall"];
-  println!("{}", seasons[3]);
-  seasons[3] = "Autumn"; //changing out the 3rd element in the array
-  println!("{}", seasons[3]);   
 }
