a283c588e1acf41e2e71b06cfa013fd6dee274ed
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-23 08:11:57 -0500

    Explicit and Implicit Return

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
-  //functions with parameters and passing in arguments
+//Return Types, Explicit and Implicit
 fn main() {
-    open_store("Gainesville");
-    baking_pizza(20, "pepperoni");
-    making_money();
+    let total = square(5);
+    println!("{}", total);
+
+    let result = cubed(4);
+    println!("{}", result);
 }
 
-fn open_store (neighborhood: &str) {
-    println!("I am going to open a pizza store in {}", neighborhood);
-    }
-fn baking_pizza (number: u8, toppings: &str) {
-    println!("I bake {} pizzas a day on average, with {} toppings on each.", number, toppings);
-    }
-
-fn making_money () {
-    println!("I am making money hand over fist");
-    }
+fn square(number: i8) -> i8 {
+    //must declare the return type
+    return number * number; //explicit returns require the key word 'return' and the line is finished with a ;
+}
+fn cubed(number: u8) -> u8 {
+    //must declare the return type
+    number * number * number //implicit returns do not accept ; the very last line is what you want to return and be the output
+}
