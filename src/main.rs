1869b4fbb4cc7db2c80054b110892c64dfc2b44e
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-23 08:33:39 -0500

    Assigning a variable to an if, else if statement

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,15 @@
-//If, Else If, Else
-//If statements must evaluate to a boolean, each If statement is evaluated individually
-//Else If statements stop checking once a boolean has evaluated to true
-//Else captures any other value
+//Assigning a variable to an If, Else If statement
 fn main () {
-    let number = 3;
-    if number > 4 {
-        println!("Number is greater than 4");
-    }
-    if number < 4 {
-        println!("Number is less than 4");
-    }
-
-    let season = "potato";
-    if season == "summer" {
-        println!("The season of sun is here");
-    } else if season == "winter" {
-        println!("It is getting cold outside");
-    } else if season == "spring" {
-        println!("Flowers give me allergies");
-    } else {
-        println!("Who knows??!!");
-    }
+    odd_or_even(17);
+    odd_or_even(100);
 }
 
+fn odd_or_even (number: u8) {
+    let result = if number % 2 == 0 {
+       "even"
+    } else {
+        "odd"
+    };
+    println!("The number is {}", result);
+
+}
