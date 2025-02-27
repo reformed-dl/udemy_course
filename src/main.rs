e3d392e0c75ef49322db11962b176272c87cc989
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-02-27 06:03:55 -0500

    Ownership with Mutable Parameters, Return Values

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,27 @@
-//Ownership, Copy, Move and String Concatenation
+//Ownership with Mutable Parameters, Return Values
 fn main () {
-    let name: String = String::from("Tyson"); 
-    println!("{}", name);
-    let boxer = &name;//&references name, address to the same piece of data on the heap. Both name and boxer are valid
-    println!("{}", boxer);
-    println!("{}", name);
-    let fighter = name;//Ownership is MOVED from name to fighter. Name is no longer valid after this point, nor is boxer
-    println!("{}", fighter);    
-    //println!("{}", boxer);
-    //println!("{}", name);
+    let ribs = String::from("smoked ribs");
+    lunch(ribs); //call lunch fn, burger variable is passed in as an argument, let meal = ribs, then " and corn on the cobb" is added to the end of the String
 
-    //slices of Strings
-    let s: String = String::from("Mighty Mouse");
-    let t = &s;
-    let v = &s[0..6];
-    let z = &s[7..];
-    println!("{}", s);
-    println!("{}", t);
-    println!("{}", v);
-    println!("{}", z);
+    let legs = workout();//legs = implicit return value of the workout() fn
+    println!("Set PBs in {} today", legs);
+    println!("{}", workout()); //workout() is still usable
 
-    //Concatenation on Strings
-    let mut family: String = String::new();
-    family.push_str("Father");
-    println!("{}", family);
-    let woman: String = String::from(", Mother");
-    family.push_str(&woman);
-    println!("{}", family);
-    let kids = " and Children";
-    family.push_str(kids);
-    println!("{}", family);
-
-    //Copy Vs Move in fn parameters
-    let value = 21;
-    immutable(value);//let num = value; COPY is made. 
-    println!("{} value is still useable", value);
-
-    let nation = String::from("United States");
-    dynamic(nation);//let country = nation; Ownership is moved.
-    //cannot use nation as it is no longer valid
-
-    let city = String::from("Washington DC");
-    dynamic_two(&city);//parameter asks for a ref to a String, no copy is made, city is still useable.
-    println!("{} is still useable", city);
-
-    let king = String::from("Christ");
-    dynamic_three(king.clone()); //using .clone() method makes a copy and king is still useable.
-    println!("{} is still useable", king);
-
+    let cake = bake_cake();//cake = explicit return value of the back_cake() fn
+    println!("I have a {} cake", cake);
+    println!("{}", bake_cake());//bake_cake() is still usable
 }
 
-fn immutable (num: i32) {
-    println!("{} is an immutable data type", num);
+fn lunch (mut meal: String) {
+    meal.push_str(" and corn on the cobb"); //take whatever value is given for meal and add " and corn on the cobb" to the end of it
+    println!("{} is on the menu for lunch today", meal);
 }
 
-fn dynamic (country: String) {
-    println!("{} is where I live", country);
+fn workout () -> String {
+    String::from("Deadlifts and Squats") //this function provides an implicit return of this String Value
 }
 
-fn dynamic_two (place: &String) {
-    println!("{} is the capital of the USA", place);
-}
-
-fn dynamic_three (lord: String) {
-    println!("{} is King of Creation", lord);
+fn bake_cake () -> String {
+    let cake = String::from("Chocolate Mouse");
+    return cake; //this function provides an explicit return of the String Value
 }
