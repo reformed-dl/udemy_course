ca3328df20492f38a98db7cd7df02db42f9d5997
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-02 08:57:04 -0500

    Creating Structs in a Function

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,41 @@
-/* Structs Basics
-Structs are containers used to store related data
-3 types of Structs: 1. Named Field 2. Tuple-like 3. Unit-like
-Struct Fields are identified by their name and they have a corresponding value
-fields are the owners of their values, structs are the owner of the fields
-First defifne a blueprint of the struct and then an instance of the struct in the actual program*/
+/*Creating Structs in a Function
+Structs can serve as return values. Moving the Struct outside of the main fn, allows it to be used outside of the scope of main. 
+Now that it exists at the file level, it allows us to refer to this struct within any function in this file. More Global, More Available.
+ */
+struct Coffee {
+    price: f32,
+    name: String,
+    is_hot: bool,
+}
 
 fn main() {
-    struct CoffeDrink {//This is the Bluepring of the Struct we will instantiate in the program
-        name: String,//field names and the types of data that will correspond to each field
-        price: f32,
-        is_hot: bool,
-    }
+    let coffee = make_coffee(String::from("Espresso"), 5.99, true);//because Coffee is returned, it is not lost and coffee becomes the new owner of the values of the struct instance
+    println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
 
-    let americano = CoffeDrink {//This is creating an instance of the Struct
-        name: String::from("Americano"),
-        price: 5.99,
-        is_hot: true,
-    };//need an ';' here
-    //access a struct field by struct_name.field_name
-    println!("{}", americano.name);
-    println!("{}", americano.price);
-    println!("{}", americano.is_hot);
-    println!("This morning I picked up an {} for {}. It is {} that it was hot.", americano.name, americano.price, americano.is_hot);
-
-    //let favorite_drink = americano.name; //ownership is moved here, since name field is a String and stored on the heap, name is no longer viable and usble
-    //println!("{}", favorite_drink);
-
-    //Overwriting Struct Fields
-    struct ActionHero {//template is not mutable or immutable
-        name: String,
-        display_character: char,
-        starting_lives: u8,
-    }
-
-    let mut terminator = ActionHero {//the instance itself needs to be declared mutable, if mutable all fields are mutable. All or nothing
-        name: String::from("Terminator"),
-        display_character: '@',
-        starting_lives: 8,
+    let name = String::from("Latte");//This is reassigning the field name values
+    let price = 3.99;
+    let is_hot = false;
+    let latte = Coffee {//calling a new instance of the Coffee Struct
+        name,
+        price,
+        is_hot,
     };
-    println!("My fovrite video game is {}, the character on the map is shown as {} and you have {} starting lives.", terminator.name, terminator.display_character, terminator.starting_lives);
+    println!("My {} cost {} and it is {} that it was hot.", latte.name, latte.price, latte.is_hot);
 
-    terminator.name = String::from("Arnold");//access the existing struct name and field. struct_name.field_name and set it to new value
-    terminator.display_character = '$';
-    terminator.starting_lives = 5;
+    let coffee = make_coffee(String::from("Espresso"), 5.99, true);//I can still use this here because I am just calling a new instance of the Coffee Struct
+    println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
 
-    println!("{}, {}, {}", terminator.name, terminator.display_character, terminator.starting_lives);
+}
+
+fn make_coffee(name: String, price: f32, is_hot: bool) -> Coffee {//I can return any valid type, the struct Coffe is now a valid type to be implicity returned
+    Coffee {
+        name: name,//first name corresponds to the name field in the coffee struct, the second name corresponds to the name parameter that will be passed in
+        price: price,//first price corresponds to the price field in the coffe struct, the second price corresponds to the price parameter that will be passed in
+        is_hot: is_hot,//the first is_hot corresponds the the field name, the second is_hot corresponds to the parameter that will be passed in
+        /*you can use a shortcut for syntax when the stuct field and that parameter names are the same
+        name,
+        price,
+        is_hot,
+        and this will work, but the field name and the parameter name must be identical */
+    }
 }
