f147b784360194020b5c37d7c67ecfc1fe0b2775
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-03 05:25:46 -0500

    String Values in Structs/clone()

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,41 @@
-/*Creating Structs in a Function
-Structs can serve as return values. Moving the Struct outside of the main fn, allows it to be used outside of the scope of main. 
-Now that it exists at the file level, it allows us to refer to this struct within any function in this file. More Global, More Available.
- */
-struct Coffee {//This defines the blueprint for the Coffee struct that I can use in various ways. Examples below
+//String values in Structs are still stored on the heap and you have to be careful for ownership issues.
+struct Coffee {
     price: f32,
     name: String,
     is_hot: bool,
 }
 
 fn main() {
-    let coffee = make_coffee(String::from("Espresso"), 5.99, true);//because Coffee struct is returned, I can call make_coffee and pass in arguments for struct fields
-    println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
+    let coffee = make_coffee(String::from("Espresso"), 5.99, true);
 
-    let name = String::from("Latte");//This is reassigning the field name values
-    let price = 3.99;
-    let is_hot = false;
-    let latte = Coffee {//calling a new instance of the Coffee Struct
-        name,
-        price,
-        is_hot,
+    let mocha = Coffee {
+        name: String::from("Mocha"),
+        ..coffee//this tells Rust to copy the values from the coffee variable
     };
-    println!("My {} cost {} and it is {} that it was hot.", latte.name, latte.price, latte.is_hot);
 
-    let mocha = make_coffee(String::from("Mocha"), 5.24, true);
-    println!("My {} cost {} and it is {} that it was hot.", mocha.name, mocha.price, mocha.is_hot);
+    let coffee_two = Coffee {
+        ..coffee//thsi copies all of the values in the coffee instance of the struct, and since we have a String, will make coffee.name no longer usable
+    };
 
-    //This shows that I can instantiate the Coffee struct as many times as I want, by either calling the make_coffee fn or setting a variable equal to the struct
-    //passing in different values for the fields and still get valid print outs.
+    println!("{}", mocha.name);
+    println!("{}", coffee_two.name);
+    //println!("{}", coffee.name); can't use coffee here becuase the name value moved to coffee_two
 
-    println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
-    println!("My {} cost {} and it is {} that it was hot.", latte.name, latte.price, latte.is_hot);
+    //in order to continue to use the string value of an instance after copying the values, use clone()
+
+    let coffee_three = Coffee {
+        name: coffee_two.name.clone(),
+        ..coffee_two
+    };
+    println!("{}", mocha.name);
+    println!("{}", coffee_two.name);
+    println!("{}", coffee_three.name);   
 }
 
-fn make_coffee(name: String, price: f32, is_hot: bool) -> Coffee {//I can return any valid type, the struct Coffe is now a valid type to be implicity returned
+fn make_coffee(name: String, price: f32, is_hot: bool) -> Coffee {
     Coffee {
-        name: name,//first name corresponds to the name field in the coffee struct, the second name corresponds to the name parameter that will be passed in
-        price: price,//first price corresponds to the price field in the coffe struct, the second price corresponds to the price parameter that will be passed in
-        is_hot: is_hot,//the first is_hot corresponds the the field name, the second is_hot corresponds to the parameter that will be passed in
-        /*you can use a shortcut for syntax when the stuct field and that parameter names are the same
         name,
         price,
         is_hot,
-        and this will work, but the field name and the parameter name must be identical */
     }
 }
