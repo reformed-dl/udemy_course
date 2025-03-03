7d452503ed01773907f77bc88b7cfe01cf95253a
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-03 04:52:09 -0500

    Structs in a Function 2.0

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 Structs can serve as return values. Moving the Struct outside of the main fn, allows it to be used outside of the scope of main. 
 Now that it exists at the file level, it allows us to refer to this struct within any function in this file. More Global, More Available.
  */
-struct Coffee {
+struct Coffee {//This defines the blueprint for the Coffee struct that I can use in various ways. Examples below
     price: f32,
     name: String,
     is_hot: bool,
 }
 
 fn main() {
-    let coffee = make_coffee(String::from("Espresso"), 5.99, true);//because Coffee is returned, it is not lost and coffee becomes the new owner of the values of the struct instance
+    let coffee = make_coffee(String::from("Espresso"), 5.99, true);//because Coffee struct is returned, I can call make_coffee and pass in arguments for struct fields
     println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
 
     let name = String::from("Latte");//This is reassigning the field name values
@@ -22,9 +22,14 @@ fn main() {
     };
     println!("My {} cost {} and it is {} that it was hot.", latte.name, latte.price, latte.is_hot);
 
-    let coffee = make_coffee(String::from("Espresso"), 5.99, true);//I can still use this here because I am just calling a new instance of the Coffee Struct
-    println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
+    let mocha = make_coffee(String::from("Mocha"), 5.24, true);
+    println!("My {} cost {} and it is {} that it was hot.", mocha.name, mocha.price, mocha.is_hot);
 
+    //This shows that I can instantiate the Coffee struct as many times as I want, by either calling the make_coffee fn or setting a variable equal to the struct
+    //passing in different values for the fields and still get valid print outs.
+
+    println!("My {} cost {} and it is {} that it was hot.", coffee.name, coffee.price, coffee.is_hot);
+    println!("My {} cost {} and it is {} that it was hot.", latte.name, latte.price, latte.is_hot);
 }
 
 fn make_coffee(name: String, price: f32, is_hot: bool) -> Coffee {//I can return any valid type, the struct Coffe is now a valid type to be implicity returned
