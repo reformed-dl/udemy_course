94dedf4ff0ecadf73c5c89c9a6dfedd282b16fae
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-05 19:59:17 -0500

    Methods on a Struct (2)

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,27 @@ struct Events {
     who: bool,
 }
 
-        /*Parameter Types: 1st parameter must always be self.
-        4 Options: 
-        1) Immutable struct value, self parameter takes ownership (self or self: Self)
-        2) Mutable struct value, self parameter takes ownership and can modify (mut self or self: mut Self)
-        3) Immutable reference to a struct instance, no ownership (&self or self: &Self)
-        4) Mutable reference to a struct instance, no ownership but can modify (&mut self or self: &mut Self) */
-
 impl Events {
-    fn what_event(self: Self) {
-        println!("{}", self.what);
+    fn what_event(self: &Self) {//ownership is not taken, can also be written (&self)
+        println!("{}", self.what);//self.field_name
         println!("The year that the wheels fell off? {}", self.when);
         println!("Has it gotten worse? {}", self.who);
+    }
+
+    fn double_when(self: &mut Self) {//ownership is not taken, but able to modify. Can also be written (&mut self)
+        self.when = self.when * 2;
+        println!("New date is {}", self.when);
     }
 }
 
 fn main() {
-    let event = Events {
+    let mut event = Events {//had to change to mut event for double_when function
         what: String::from("The West turned fake and gay"),
         when: 1971,
         who: true,
     };
 
     event.what_event();//the fn is called on the end of the variable. self parameter is automatically passed in. In this case, ownership is taken by the self parameter.
+    event.double_when();//when called, it updates the self.when field.
+    event.what_event();//date has been changed on this print out due to double_when modifying the struct instance, but what_event is still valid and usable.
 }
