ca5ac011b4190590262a0e08f6a9d4a6d5a5de05
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-06 05:34:31 -0500

    Associated fns, Constructors in Structs

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,38 @@
-//Calling Methods from other methods
+//Associated functions, constructors
+//Associated functions are functions attached to a type, example String::from() and String::new(), these are functions attached to the String type, not methods.
+//Often use associated function for constructors. Constructor is a function that returns a new instance of the type
+//will always be fn new()
+//written inside of the impl block, but Rust can differentiate between a method and an associated function. Methods will always have 'self' in the parameter
 #[derive(Debug)]
 struct Events {
     what: String,
     when: u16,
     who: bool,
 }
-
 impl Events {
-    fn what_event(self: &Self) {
+    fn new(what: String, when: u16, who: bool) -> Self {//this will construct a new instance of the struct
+        Self {
+            what,
+            when,
+            who,
+        }
+    }   
+    fn what_event(self: &Self) {//method to be called in main
         println!("{}", self.what);
-        println!("It has been {} years since the event took place", self.years_since_event());//calling a method instead of a field
+        println!("Kicked off in {}", self.when);
         println!("Was it the Jews? {}", self.who);
     }
+}
 
-    fn years_since_event(&self) -> u16 {
-        2025 - self.when
-    }
+fn main() { 
+    let event = Events::new(String::from("World War II Began"), 1939, true);//'::' accesses the associated function, then pass in arguments for new instance of struct
+    println!("{:#?}", event);
 
-    //fn happened_before(&self, other: &Self) -> bool {//returns a bool value comparing the when value of self and other
-       // self.when < other.when
-   // }
-
-   // fn add_text(&mut self, other: &mut Self) {//this function calls in both instances and concatenates text to the end of the when field
-       // self.what.push_str(" and it gets gayer every year");
-       // other.what.push_str(" was started by you know who");
-       // println!("{}", self.what);
-       // println!("{}", other.what);
-       // println!("{:#?}, {:#?}", self, other);//prints out full struct instance
-   // }
-//}
-
-//fn struct_return(what: String, when: u16, who: bool) -> Events {
-  //  Events {
-    //    what,
-    //    when,
-    //    who,
-   // }
-//}
-
-fn main() {
-    let event = Events {
+    let event_two = Events {
         what: String::from("The West turned fake and gay"),
         when: 1971,
         who: true,
     };
-
-    event.what_event();
-
-
-   // let mut event_two = Events {
-      //  what: String::from("World War II"),
-      //  when: 1939,
-      //  who: true,
-   // };
-
-   // event.what_event();
-
-   // match event.happened_before(&event_two) {//called happened_before on event variable. This value is passed in automatically for self, &event_two passed in for other parameter
-    //   true => println!("{} happened before {}", event.what, event_two.what),
-    //    false => println!("{} happened before {}", event_two.what, event.what),
-  //  };
-
-  // event.add_text(&mut event_two);
-
-  // let bitcoin = struct_return(String::from("The West Is Saved"), 2009, true);
-  // println!("Because of Bitcoin, in {}, {}? {}", bitcoin.when, bitcoin.what, bitcoin.who);
+    event_two.what_event();
+    event.what_event();//can also call these methods on the new instance
 }
