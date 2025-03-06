c983887f6bf224fd5512f5e5e873933e1475dbcd
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-05 20:41:39 -0500

    Methods on a Struct (3)

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//Methods on a Struct
+//Methods on a Struct (3) integrating match statements, returns and concatentation
 #[derive(Debug)]
 struct Events {
     what: String,
@@ -7,26 +7,55 @@ struct Events {
 }
 
 impl Events {
-    fn what_event(self: &Self) {//ownership is not taken, can also be written (&self)
-        println!("{}", self.what);//self.field_name
+    fn what_event(self: &Self) {
+        println!("{}", self.what);
         println!("The year that the wheels fell off? {}", self.when);
         println!("Has it gotten worse? {}", self.who);
     }
 
-    fn double_when(self: &mut Self) {//ownership is not taken, but able to modify. Can also be written (&mut self)
-        self.when = self.when * 2;
-        println!("New date is {}", self.when);
+    fn happened_before(&self, other: &Self) -> bool {//returns a bool value comparing the when value of self and other
+        self.when < other.when
+    }
+
+    fn add_text(&mut self, other: &mut Self) {//this function calls in both instances and concatenates text to the end of the when field
+        self.what.push_str(" and it gets gayer every year");
+        other.what.push_str(" was not the last world war");
+        println!("{}", self.what);
+        println!("{}", other.what);
+        println!("{:#?}, {:#?}", self, other);//prints out full struct instance
+    }
+}
+
+fn struct_return(what: String, when: u16, who: bool) -> Events {
+    Events {
+        what,
+        when,
+        who,
     }
 }
 
 fn main() {
-    let mut event = Events {//had to change to mut event for double_when function
+    let mut event = Events {
         what: String::from("The West turned fake and gay"),
         when: 1971,
         who: true,
     };
 
-    event.what_event();//the fn is called on the end of the variable. self parameter is automatically passed in. In this case, ownership is taken by the self parameter.
-    event.double_when();//when called, it updates the self.when field.
-    event.what_event();//date has been changed on this print out due to double_when modifying the struct instance, but what_event is still valid and usable.
+    let mut event_two = Events {
+        what: String::from("World War II"),
+        when: 1939,
+        who: true,
+    };
+
+    event.what_event();
+
+    match event.happened_before(&event_two) {//called happened_before on event variable. This value is passed in automatically for self, &event_two passed in for other parameter
+        true => println!("{} happened before {}", event.what, event_two.what),
+        false => println!("{} happened before {}", event_two.what, event.what),
+    };
+
+   event.add_text(&mut event_two);
+
+   let bitcoin = struct_return(String::from("The West Is Saved"), 2012, true);
+   println!("Because of Bitcoin, in {}, {}? {}", bitcoin.when, bitcoin.what, bitcoin.who);
 }
