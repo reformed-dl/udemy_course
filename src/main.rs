e4fafe383114fe3063247e392b8a770a71204b2a
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-07 08:25:00 -0500

    Builder Pattern

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,41 @@
-//Associated functions, constructors
-//Associated functions are functions attached to a type, example String::from() and String::new(), these are functions attached to the String type, not methods.
-//Often use associated function for constructors. Constructor is a function that returns a new instance of the type
-//will always be fn new()
-//written inside of the impl block, but Rust can differentiate between a method and an associated function. Methods will always have 'self' in the parameter
+//Builder Pattern
 #[derive(Debug)]
-struct Events {
-    what: String,
-    when: u16,
-    who: bool,
+struct CarLot {
+    name: String,
+    price: u32,
+    miles: u32,
 }
-impl Events {
-    fn new(what: String, when: u16, who: bool) -> Self {//this will construct a new instance of the struct
+
+impl CarLot {
+    fn new(name: String, price: u32, miles: u32) -> Self {//Creates a new instance of the CarLot struct
         Self {
-            what,
-            when,
-            who,
+            name,
+            price,
+            miles,
         }
-    }   
-    fn what_event(self: &Self) {//method to be called in main
-        println!("{}", self.what);
-        println!("Kicked off in {}", self.when);
-        println!("Was it the Jews? {}", self.who);
+    }
+
+    fn new_car(&mut self, new_car: String) -> &mut Self {
+        self.name = new_car;//overwrites field_value with new value
+        self//returns the modifed value
+    }
+
+    fn new_price(&mut self, new_price: u32) -> &mut Self {
+        self.price = new_price;
+        self
+    }
+
+    fn new_miles(&mut self, new_miles: u32) -> &mut Self {
+        self.miles = new_miles;
+        self
     }
 }
-
 fn main() { 
-    let event = Events::new(String::from("World War II Began"), 1939, true);//'::' accesses the associated function, then pass in arguments for new instance of struct
-    println!("{:#?}", event);
+    let mut car = CarLot::new(String::from("Ford F-250"), 35000, 10298);//set variable to new instance of the struct
+    println!("Car Details: {:#?}", car);
 
-    let event_two = Events {
-        what: String::from("The West turned fake and gay"),
-        when: 1971,
-        who: true,
-    };
-    event_two.what_event();
-    event.what_event();//can also call these methods on the new instance
+    //builder pattern allows me to string together methods
+    //Only possible due to how the methods are designed in the impl block
+    car.new_car(String::from("Jeep CJ7")).new_price(25999).new_miles(89200);
+    println!("Car Details: {:#?}", car);
 }
