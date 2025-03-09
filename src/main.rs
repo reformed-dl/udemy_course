9539eb7ff51a4efef50354b7761f383ad7b5fb3e
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-09 08:19:09 -0400

    Enums and Match Statements (1)

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
-//Nesting Enums in Enums, Enums as Associated Fields.
+//Enums and Match Statements
+//Match Statements work well with Enums as Enums are a specific set of possible variants
 #[derive(Debug)]
-enum Beans {
-    Pinto,
-    Black,
+
+enum OperatingSystem {
+    Windows,
+    MacOS,
+    Linux,
 }
 
-#[derive(Debug)]
-enum Meat {
-    Chicken,
-    Steak,
+fn years_since_release(os: OperatingSystem) -> u32 {//parameter is set to the type of enum and returns a u32 int value
+    match os {
+        OperatingSystem::Windows => 39,//each arm represents one of the enum variants and returns a u32 int value
+        OperatingSystem::MacOS => 23,
+        OperatingSystem::Linux => 34,
+    }
 }
 
-#[derive(Debug)]
-enum RestaurantItem {
-   Burrito{meat: Meat, bean: Beans},//using 2 enums as an associated field, instead of (), use {} and set up as a named field struct
-   Bowl{meat: Meat, bean: Beans},
-   VeganPlate,
+fn main () {
+    let my_computer = OperatingSystem::Linux;//set a variable equal to one of the enum variants
+    let age = years_since_release(my_computer);//set a variable equal to the fn with the variable passed in as the argument (variable equals the enum variant)
+    println!("My computer's operating system is {} years old", age);
 }
-
-fn main() {
-    let lunch = RestaurantItem::Burrito { meat: Meat::Steak, bean: Beans::Black };
-    let dinner = RestaurantItem::Bowl { meat: Meat::Chicken, bean: Beans::Pinto };
-    let abandoned_meal = RestaurantItem::VeganPlate;
-    println!("Lunch was: {:?}", lunch);
-    println!("Dinner was: {:?}", dinner);
-    println!("The meal nobody chose was: {:?}", abandoned_meal);
-
-}   
-   
