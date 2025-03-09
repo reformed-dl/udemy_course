a53dfbacfaaac58c1e3802e74e623b7ec8e07742
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-09 09:08:51 -0400

    Methods on Enums (1)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
-//Enums and Match Statements (3)
+//Methods on Enums (1)
 #[derive(Debug)]
 enum LaundryCycles {
     Cold,
-    Hot{temp: u32},//struct named field associated value
-    Delicate(String),//tuple like associated value
+    Hot{temp: u32},
+    Delicate(String),
 }
 
-fn wash_laundry(temperature: LaundryCycles) {
-    match temperature {
-        LaundryCycles::Cold => {
-            println!("Washing my laundry with cold water");
-        }
-        LaundryCycles::Hot { temp } => {//here we are extracting our fields by name
-            println!("Washing my laundry with {} degree water", temp);
-        }
-        LaundryCycles::Delicate(fabric) => {//choose whatever name I want to represent the String, here we are extracting our values by order or position
-            println!("Washing my {} laundry on the delicate cylce", fabric);
+impl LaundryCycles {
+    fn wash_laundry(&self) {
+        match self {
+            LaundryCycles::Cold => {
+                println!("Washing my laundry with cold water");
+            }
+            LaundryCycles::Hot { temp } => {
+                println!("Washing my laundry with {} degree water", temp);
+            }
+            LaundryCycles::Delicate(fabric) => {
+                println!("Washing my {} laundry on the delicate cylce", fabric);
+            }
         }
     }
 }
+
 fn main () {
-    wash_laundry(LaundryCycles::Cold);
-    wash_laundry(LaundryCycles::Hot { temp: 100 });//input the 100
-    wash_laundry(LaundryCycles::Delicate(String::from("Silk Underoos")));//input the String Value
+   LaundryCycles::Cold.wash_laundry();
+   let hot_cycle = LaundryCycles::Hot { temp: 100 };
+   hot_cycle.wash_laundry();
+
+   let delicate_cycle = LaundryCycles::Delicate(String::from("Silk Underoos"));
+   delicate_cycle.wash_laundry();
+
+   LaundryCycles::Hot { temp: 100 }.wash_laundry();
+   LaundryCycles::Delicate(String::from("Silk Underoos")).wash_laundry();
 }
     
