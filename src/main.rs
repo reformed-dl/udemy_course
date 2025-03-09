94d799f5519ce56b8f0fd9b1f18863e6e4425f95
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-09 07:59:55 -0400

    Nesting Enums in Enums, Enums as Associated Fields

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-//Nesting Enums in Enums
+//Nesting Enums in Enums, Enums as Associated Fields.
+#[derive(Debug)]
+enum Beans {
+    Pinto,
+    Black,
+}
+
 #[derive(Debug)]
 enum Meat {
     Chicken,
@@ -7,14 +13,14 @@ enum Meat {
 
 #[derive(Debug)]
 enum RestaurantItem {
-   Burrito(Meat),//using the enum above as the associated filed for this variant
-   Bowl(Meat),
+   Burrito{meat: Meat, bean: Beans},//using 2 enums as an associated field, instead of (), use {} and set up as a named field struct
+   Bowl{meat: Meat, bean: Beans},
    VeganPlate,
 }
 
 fn main() {
-    let lunch = RestaurantItem::Burrito(Meat::Steak);
-    let dinner = RestaurantItem::Bowl(Meat::Chicken);
+    let lunch = RestaurantItem::Burrito { meat: Meat::Steak, bean: Beans::Black };
+    let dinner = RestaurantItem::Bowl { meat: Meat::Chicken, bean: Beans::Pinto };
     let abandoned_meal = RestaurantItem::VeganPlate;
     println!("Lunch was: {:?}", lunch);
     println!("Dinner was: {:?}", dinner);
