9d288ac2fcfaa359546c5ec3a205d520066c0342
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-11 05:54:57 -0400

    Enums If Let Construct

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,30 @@
-//Methods on Enums (3) Exact Match
+//Enums If Let Construct
+//Match keyword is exhaustive, it has to cover every possible variant
+//we can use the _ wildcard, but if we only have one variant that we want to run code for, we can use if let construct 
 #[derive(Debug)]
 enum Milk {
     Lowfat(i32),
     Whole,
+    NonDairy{kind: String},
 }
 
-impl Milk {
-    fn kind(&self) {
-        match self {
-            Milk::Lowfat(2) => {//determine exact match i32 value
-                println!("2% Milk is my favorite");
-            }
-            Milk::Lowfat(percent) => {//'percent' is whatever name I choose, this will satisfy match for all other i32 values
-                println!("{:?} Milk is not good", percent);
-            }
-            Milk::Whole => {
-                println!("Whole Milk has too much fat");
-            }
-        }
-    }
-}
-   
+
 fn main () {
- Milk::Lowfat(1).kind();
- Milk::Lowfat(2).kind();
- Milk::Whole.kind();
+    let my_beverage = Milk::Whole; //variable assignment to an enum variant
+    //variable is hardcoded now, but if it was dynamically entered and we wanted to only run code if the input met a certain value
+    if let Milk::Whole = my_beverage {//opposite construction as we would normally assign variable values
+        println!("You have whole Milk");
+    }
+
+    let wife_beverage = Milk::Lowfat(2);//define variable and determine i32 value
+    if let Milk::Lowfat(percent) = wife_beverage {//percent was determined by me and becomes the variable that is interpolated in println!
+        println!("Wife's beverage is {}% milk", percent);//2 is passed in from the associated data of the variant
+    }
+
+    let kid_beverage = Milk::NonDairy { kind: String::from("Oat Milk") };//define the String Value in the variant associated data
+    if let Milk::NonDairy { kind } = kid_beverage {//'kind' is provided from the struct associated data and becomes what is interpolated in println!
+        println!("Kid's beverage is {}", kind);//'Oat Milk' is passed in from the struct associated data of the variant
+    }
 }
 
     
