7ef2821c77bd5e738afd02fcbf51c813d6c0843a
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-12 04:23:38 -0400

    Enums Let Else Construction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-//Enums If Let Construct
-//Match keyword is exhaustive, it has to cover every possible variant
-//we can use the _ wildcard, but if we only have one variant that we want to run code for, we can use if let construct 
+//Enums Let Else Construct
+//Opposite contruction to the let if construct
 #[derive(Debug)]
 enum Milk {
     Lowfat(i32),
@@ -10,21 +9,28 @@ enum Milk {
 
 
 fn main () {
-    let my_beverage = Milk::Whole; //variable assignment to an enum variant
-    //variable is hardcoded now, but if it was dynamically entered and we wanted to only run code if the input met a certain value
-    if let Milk::Whole = my_beverage {//opposite construction as we would normally assign variable values
-        println!("You have whole Milk");
-    }
+    //Example of when the variable declaration and the let else matches, else block of code will not run
+    let wife_beverage = Milk::Lowfat(2);//set variable declaration
+    let Milk::Lowfat(percent) = wife_beverage else {//this code will not run and entire block will be skipped because variable declaration is equal to Milk::Lowfat
+        println!("You do have whole milk");
+        return;
+    };
 
-    let wife_beverage = Milk::Lowfat(2);//define variable and determine i32 value
-    if let Milk::Lowfat(percent) = wife_beverage {//percent was determined by me and becomes the variable that is interpolated in println!
-        println!("Wife's beverage is {}% milk", percent);//2 is passed in from the associated data of the variant
-    }
+    println!("You have {}% milk", percent);//This code will run and 'perecent' is still usable down here
 
-    let kid_beverage = Milk::NonDairy { kind: String::from("Oat Milk") };//define the String Value in the variant associated data
-    if let Milk::NonDairy { kind } = kid_beverage {//'kind' is provided from the struct associated data and becomes what is interpolated in println!
-        println!("Kid's beverage is {}", kind);//'Oat Milk' is passed in from the struct associated data of the variant
-    }
+    //Example of when the variable declaration and the let else does not match, else block of code will run
+    let my_beverage = Milk::Whole;//set variable declaration
+    let Milk::Lowfat(percent) = my_beverage else {//this code will run because variable does not equal Milk::Lowfat
+        println!("You do not have lowfat milk");
+        return;//we need to add return; to tell the code to stop running or it will continue and provide an error 
+    };
+
+    println!("You have {}% milk", percent);//this would be available if else condition and corresponding code was not run previously
 }
+   
+    
+
+
+    
 
     
