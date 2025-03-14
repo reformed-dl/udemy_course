7567455172fcc56861bd9504b8fec7568bbd02ae
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-14 06:26:42 -0400

    Generics Intro

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,45 @@
-//Enums Let Else Construct
-//Opposite contruction to the let if construct
-#[derive(Debug)]
-enum Milk {
-    Lowfat(i32),
-    Whole,
-    NonDairy{kind: String},
-}
+/*Generics are a Type Argument.
+An abstract placeholder type that stands in for a future concrete type
+Think about parameter names in functions. They add flexibility for something concrete that will be passed in later. 
+fn add_5 (value: i32) -> i32 {
+    value + 5
+} value name parameter for an i32 Type. we don't know what the argument will be for the value parameter, 
+ but the parameter name allows us define a placeholder for an expected future input of any i32 value.
+ Generics function in a similar way but for Types not a value.
+ 
+ Turbofish Operator ::<Type>, when invoking a function with a generic type, can include the Turbofish operator to specify the exact type. This can help ensure type safety*/
 
-
-fn main () {
-    //Example of when the variable declaration and the let else matches, else block of code will not run
-    let wife_beverage = Milk::Lowfat(2);//set variable declaration
-    let Milk::Lowfat(percent) = wife_beverage else {//this code will not run and entire block will be skipped because variable declaration is equal to Milk::Lowfat
-        println!("You do have whole milk");
-        return;
-    };
-
-    println!("You have {}% milk", percent);//This code will run and 'perecent' is still usable down here
-
-    //Example of when the variable declaration and the let else does not match, else block of code will run
-    let my_beverage = Milk::Whole;//set variable declaration
-    let Milk::Lowfat(percent) = my_beverage else {//this code will run because variable does not equal Milk::Lowfat
-        println!("You do not have lowfat milk");
-        return;//we need to add return; to tell the code to stop running or it will continue and provide an error 
-    };
-
-    println!("You have {}% milk", percent);//this would be available if else condition and corresponding code was not run previously
-}
-   
-    
-
-
+ #[derive(Debug)]
+ struct DeliSandwich {
+     name: String,
+     price: f32,
+ }
+ fn main () {
+     println!("{}", identity(5));
+     println!("{}", identity::<u8>(5));//Turbofish to specify u8 Type
+     println!("{}", identity(13.14));
+     println!("{}", identity::<f32>(13.14));//Turbofish to specify f32 Type
+     println!("{}", identity("hello"));
+     println!("{}", identity(String::from("hello")));
+     println!("{}", identity(true));
+     println!("{:?}", identity(DeliSandwich{name: String::from("Reuben"), price: 5.99}));
+     println!("{:?}", identity::<DeliSandwich>(DeliSandwich{name: String::from("Reuben"), price: 5.99}));//Turbofish to specify DeliSandwich Struct Type
+ 
+ 
+ }
+ 
+ fn identity_i32(value: i32) -> i32 {//can only receive an i32 argument, because we have hard coded the type
+     value
+ }
+ 
+ fn identity_bool(value: bool) -> bool {//can only receive a bool argument, because we have hard coded the type
+     value
+ }
+ //the syntax to declare a Generic is <WhateverNameWeWant>, community convention is T
+ //this syntax tells rust that there will be some Type that will be identified when the function is invoked
+ fn identity<T>(value: T) -> T {
+     value
+ }
     
 
     
