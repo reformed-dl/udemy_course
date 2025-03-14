3c371a0fd63de141ee47b627690123232319833e
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-14 07:09:44 -0400

    Generics in Structs

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,28 @@
-//Multiple Generics
+//Generics is Structs
 
-//Combining Generic and Concrete Parameter Types
-fn make_tuple<T>(first: T, second: u8) -> (T, u8) {
-    (first, second)
-}
-
-//Combining Multiple Generic of the Same Parameter Types
-fn make_second_tuple<T>(first: T, second: T) -> (T, T) {//T is a community convention, Rust recognizes that whatever first is, second must be as well 
-    (first, second)
-}
-
-//Combining Multiple Generics of Different Parameter Types
-fn make_third_tuple<T, U>(first: T, second: U) -> (T, U) {//T and U are up to us, but allows us to have multiple different types, but can be the same
-    (first, second)
+#[derive(Debug)]
+struct TreasureChest<T> {//same set up as a function
+    captain: String,
+    treasure: T,//allows us flexibility for the Type here, No need to create 3 different Structs, can re-use same struct with generic Type
 }
  fn main () {
-    println!("{:?}", make_tuple("Hello, World", 57));//argument passed in for first can be whatever we want, second is the concrete type
+    let gold_chest = TreasureChest {
+        captain: String::from("Captain Sponge Bob Square Pants"),
+        treasure: "Gold",
+    };
+    println!("{:?}", gold_chest);
 
-    println!("{:?}", make_second_tuple("Hello, World", "I am a Rust Noob"));//Types must match here as they are both type T
+    let silver_chest = TreasureChest {
+        captain: String::from("Captain Second Place"),
+        treasure: String::from("Silver"),
+    };
+    println!("{:?}", silver_chest);
 
-    println!("{:?}", make_third_tuple("Hello, World", 57));//Types can be different 
-    println!("{:?}", make_third_tuple("Hello, World", "I am a Rust Noob"));//Types can be the same
+
+    let mother_load = TreasureChest {
+        captain: String::from("Reformed and Salty"),
+        treasure: ["Gold", "Silver", "Platinum", "Bitcoin"],
+    };
+    println!("{:?}", mother_load);
+
  }
