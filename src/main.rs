ada4147af1fa59c13ea97d15b171e79f816df6c4
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-19 04:28:53 -0400

    Match Statements with Option Enums

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,36 @@
- /*Built-In Enums (Using Option Enums with get(), unwrap(), expect())
-1 of the 2 most common is the Option Enum
-The Option Enum models a scenario where a Type could be a valid value or nothing at all, values present or absent
-Other languages represent/model absent values using a null value. However, this null value has long been known to cause errors
-Option Enums are Rust's solution to this problem
-
-Option::None represents an absent value
-Option::Some(T) represents a present value; where T is the generic data type of the valid value that the Some variant will hold, T is the associated data for the Some Variant*/
+ /*Match Statements with Option Enums
+ Using the unwrap() and expect() methods are optimistic and assume that there is Some variant within our Option Enum and therefore able to extract corresponding
+ associated data, but led to a runtime error if the variant was Option::None
+ Using Match Statements allows us to deal with both Option::Some(T) and Option::None in a more elegant way as we are required to have match arms that cover all cases*/
 
 fn main () {
-    let a = Option::Some(5); //whatever is passed in becomes the concrete type for the generic type T, Rust default int type is i32
-    let b = Option::Some("Hello Rust");//&str becomes concrete type for generic type T
-    let c = Option::Some(String::from("Goodbye Rust"));//String becomes the concrete type for generic type T
-
-    //to define the specific type and not let Rust choose default
-    let d: Option<u8> = Option::Some(5);
-    //you can also use the turbofish operator
-    let e = Option::<u16>::Some(5);//turbofish inserted between Option and ::Some
-
-    let f: Option<String> = Option::None;//If using Option::None, we do have to explicitly define the concrete type in case ::None becomes ::Some
-
-    //One option to extract an element from an array is to provide index position within [], 
-    //However, this can result in errors, especially with user input, a safer method is to use the 'get method' which is an option enum
     let musical_instruments = [String::from("Guitar"), String::from("Drums"), String::from("Bass")];
 
-    let bass = musical_instruments.get(2);//the type for the generic type T is a &String(get() does not extract the string) can also be hardcoded Option<&String>
-    println!("{:?}", bass);//result will be Some("Bass"); Option::Some(T); the type returned by the get() is an Option Enum of the Sum Variant storing a &String as the associated data
+    let bass: Option<&String> = musical_instruments.get(2);//Option<&String> is the concrete type since we have a [String; 3] that we are using the get() on and results in a Option Enum
 
-    //One option to extract the data from a Option Enum is the unwrap method. Unwrap Method attempts to extract the associated data out of the Some variant
-    //This will work for Option::Some(T), however it will result in a runtime panic error if we have Option::None
-    let valid_instrument = bass.unwrap();//Since bass is Option::Some(T) -> Option<&String> -> unwrap will extract the associated data
-    println!("{}", valid_instrument); //result will be 'Bass"
+    match bass {
+        Option::Some(instrument) => println!("Playing the {}", instrument),//This arm will match. I declare the name for the associated data value 'instrument'
+        Option::None => println!("Singing with my voice"),
+    }
 
-    let drums = musical_instruments.get(1);
-    println!("{:?}", drums);//result will be Some("Drums"); Option::Some(T)
+    play(bass);
+    println!("{:?}", bass);//I can still use bass here because Rust implements the Copy Trait on Option Enums
 
-    //Another option to extract the data from an Option Enum is the expect method. Expect will provide the same results as unwrap on any Option::Some. 
-    //However, expect() allows us to customize our error message on Option::None
-    let valid_drums = drums.expect("Unable to retrieve element");//error message will only run if associated data not found, Option::None
-    println!("{}", valid_drums);//result will be 'Drums'
+    let invalid_instrument: Option<&String> = musical_instruments.get(100);
 
-    let invalid_instrument = musical_instruments.get(100);//if attempted with [] would cause a panic at run time
-    println!("{:?}", invalid_instrument);//result will be None, instead of a panic error; Option::None
-    //unwrap() will not work on Option::None, and will result in a panic at runtime
-    invalid_instrument.expect("Unable to retrieve element");//result will be 'Unable to retrieve element'
+    match invalid_instrument {
+        Option::Some(instrument) => println!("Playing the {}", instrument),
+        Option::None => println!("Singing with my voice"),//This arm will match
+    }
 
+    play(invalid_instrument);
+    println!("{:?}", invalid_instrument);//I can still use invalid_instrument here as well due to the Copy Trait
+ }
+
+ //Instead of writing duplicate code as we have above, we can refactor by writing a function that can be invoked repeatedly
+ fn play(instrument_type: Option<&String>) {
+    match instrument_type {
+        Option::Some(instrument) => println!("Playing the {}", instrument),
+        Option::None => println!("Singing with my voice"),
+    }
  }
