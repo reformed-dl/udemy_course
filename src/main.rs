68f7405e4204e53ca8018301dbc20a6d31cafe18
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-25 14:02:51 -0400

    While Let Constructors

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,30 @@
-//Nuances of Unwrap Method on Enums
-//Ownership and Borrowing concerns
-fn operation(great_success: bool) -> Result<&'static str, &'static str> {//using this syntax, allows us to avoid ownership issues
-    if great_success {
-        Ok("Success")
-    } else {
-        Err("Error")
-    }
-}
+//If Let Construct Vs While Let Constructs
 
 fn main() {
-    let my_result = operation(true);//Result<&str, &str> 
+   let mut sauces = vec!["Mayo", "Ketchup", "Ranch"];
 
-    let content = match my_result {
-        Ok(message) => message,
-        Err(error) => error,
-    };
+  //if let, hard code Some(sauce) on the left side set equal to the sauce vec on the right. pop() will pull each element from the vec
+  //and as long as there is an element there to satisfy the Some(), the print line will be ran
+   if let Some(sauce) = sauces.pop() {
+    println!("The next sauce is {}", sauce);//Will pop Ranch
+   }
 
-    println!("{}", my_result.unwrap());
-    println!("{}", content);
-    println!("{}", my_result.unwrap());
+   if let Some(sauce) = sauces.pop() {
+    println!("The next sauce is {}", sauce);//will pop Ketchup
+   }
+
+   if let Some(sauce) = sauces.pop() {
+    println!("The next sauce is {}", sauce);//will pop Mayo
+   }
+
+   if let Some(sauce) = sauces.pop() {
+    println!("The next sauce is {}", sauce);//Some() is no longer satisfied as there are no more elements in the vec, nothing will print out.
+   }
+
+   //a more elegant option is a Wile Let construction that will provide the same outputs, but will run as long as the Some() is satisfied
+   let mut records = vec!["Rolling Stones", "Led Zepplin", "CCR"];
+
+   while let Some(record) = records.pop() {
+    println!("The next record to play is: {}", record);//This will pop from the last element working towards the first and only requires one line of code.
+   }
 }
