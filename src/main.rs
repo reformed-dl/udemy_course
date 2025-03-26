f5d831ae3f43035feda09c1d24b0d751f9793894
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-03-26 15:08:55 -0400

    Vectors-Initial and Methods

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
-//If Let Construct Vs While Let Constructs
+/*Vectors - data structure that is similar to an Array. Arrays are of a homogenous type, defined order and a fixed size. The number of elements cannot change.
+Vectors allow us to have contiguous, homonegous elements, but the number of elements can change
+Vectors are stored on the heap at runtime
+
+Vec is the type and is included in the Rust Prelude
+vectors implement the debug trait*/
 
 fn main() {
-   let mut sauces = vec!["Mayo", "Ketchup", "Ranch"];
+   let pizza_diameters: Vec<i32> = Vec::new();//will return a new empty Vector, but must provide specific type annotations
+   let ball_diamters = Vec::<i32>::new();//can also define specific type annotation using the turbofish operator
+   println!("{:?}", pizza_diameters);//output will be []
+   println!("{:?}", ball_diamters);
 
-  //if let, hard code Some(sauce) on the left side set equal to the sauce vec on the right. pop() will pull each element from the vec
-  //and as long as there is an element there to satisfy the Some(), the print line will be ran
-   if let Some(sauce) = sauces.pop() {
-    println!("The next sauce is {}", sauce);//Will pop Ranch
-   }
+   //if we know the initial values that we start with, we use the vec macro
+   let records = vec!["Rolling Stones", "Doors", "Lynard Skyndard"];
+   println!("{:?}", records);
 
-   if let Some(sauce) = sauces.pop() {
-    println!("The next sauce is {}", sauce);//will pop Ketchup
-   }
+   //push() adds a single element to the end of the vector
+   let mut food = vec!["Pasta", "Steak", "Ribs"];
+   println!("{:?}", food);
+   food.push("Chicken Wings");
+   println!("{:?}", food);
 
-   if let Some(sauce) = sauces.pop() {
-    println!("The next sauce is {}", sauce);//will pop Mayo
-   }
+   //insert() allows to add an element at a certain index position
+   food.insert(1, "Mac and Cheese");
+   println!("{:?}", food);
 
-   if let Some(sauce) = sauces.pop() {
-    println!("The next sauce is {}", sauce);//Some() is no longer satisfied as there are no more elements in the vec, nothing will print out.
-   }
+   //pop() attempts to remove the last element from the vec and return it
+   //pop() is an Option Enum, Some() and None
+   let meal = food.pop();
+   println!("{:?}", meal);//output will be Some("Chicken Wings")
+   println!("{:?}", food);//one pop() has been called, the last element is now removed
 
-   //a more elegant option is a Wile Let construction that will provide the same outputs, but will run as long as the Some() is satisfied
-   let mut records = vec!["Rolling Stones", "Led Zepplin", "CCR"];
+   //remove() removes an element by the index position
+   food.remove(0);//if index does not exist, will panic
+   println!("{:?}", food);//first element has now been removed from the vector
 
-   while let Some(record) = records.pop() {
-    println!("The next record to play is: {}", record);//This will pop from the last element working towards the first and only requires one line of code.
-   }
 }
