439d20cad833b4a3e922c90b8296602aafb61c07
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-04-17 13:14:23 -0400

    Modules-Project Structure Set Up

---
+++ src/inventory.rs
@@ -0,0 +1,20 @@
+pub const FLOOR_SPACE: i32 = 10000;
+pub const MANAGER: &str = "Ivan Inventory";//in order to access this const, must use the 'pub' keyword
+
+   #[derive(Debug)]
+   pub enum ProductCategory {
+      Ladder,
+      Hammer,
+   }
+
+   #[derive(Debug)]
+   pub struct Item {
+      name: String,
+      category: ProductCategory,
+      quantity: u32,
+   }
+
+   pub fn talk_to_manager() {
+      println!("Hey {} how's your coffee?", MANAGER);
+   }
+
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,17 @@
-/*Vectors - Writing and Overwriting Elements
+/*Module: Organizational containter that encapsulates related code. Similar to a Folder on Your Computer
+ keyword is "mod" and name is written in snake case
+ Modules allow us to have duplicate names. Each duplicate name can live within it's own Modules, it's own Namespace
+ 
+ There are multiple (3) ways to declare a Module
+ 1) By declaring the mod keyword and all of the associated data within the curly braces
+ 2) By creating a file within the src folder. 
+      Example inventory.rs and then any code written within that file can be accessed by typing "mod inventory;" in main
+ 3) By creating a directory(folder): create a subfolder within src folder with the module name and a file within the sub folder named "mod.rs*/
 
-Vector Capacity - the maximum number of elements that the vector can contain*/
+ mod inventory;
+ mod orders;
 
 fn main() {
-   let led_zepplin = String::from("Led Zeppelin");
-   let ccr = String::from("CCR");
-   let pearl_jam = String::from("Pearl Jam");
-
-   let mut bands = vec![led_zepplin, ccr, pearl_jam];
-
-   bands[2] = String::from("Dire Straits");//this will overwrite an element in the vector
-   println!("{:?}", bands);
-
-   let target_band = &mut bands[1];
-   target_band.push_str("-Credence Clearwater Revival");//concatenate text to the string
-   println!("{:?}", bands);
-
-   let another_band = &bands[0];//I can use a reference here even though there is a mutable reference above, because Rust defined the lifetime of that mutable reference
-   println!("{:?}", another_band);//I could not use this before the end of the lifetime of the mutable reference or I would receive a compiler
-
-   //Vector Capacity
-   let mut seasons: Vec<&str> = Vec::with_capacity(4);//this will return an empty vector with a maximum number of 4 elements
-   println!("Length: {}, Capacity: {}", seasons.len(), seasons.capacity());//output Length: 0, Capacity: 4
-
-   seasons.push("Summer");
-   seasons.push("Fall");
-   seasons.push("Winter");
-   seasons.push("Spring");
-   println!("Length: {}, Capacity: {}", seasons.len(), seasons.capacity());//output Length: 4, Capacity: 4
-
-   seasons.push("Summer Again");
-   println!("Length: {}, Capacity: {}", seasons.len(), seasons.capacity());//output Length: 5, Capacity: 8
-   //The capacity increased due to push and the previous allocated memory for the vec with a capacity of 4 is deallocated and a new vec with the capacity of 8 is stored on the heap
-   //This is why Rust prohibits more than one mutable reference at a time, so that a reference isn't pointing to something that has been deallocated from memory
-
+  println!("The manager of our inventory is {}", inventory::MANAGER);//must use "::" to access MANAGER from within the inventory module
+  println!("The manager of our orders is {}", orders::MANAGER);
 }
