3a16b6fb442f9ab56c8796c93722ecbdd5d68459
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-13 15:54:28 -0400

    Format Macro, Common Methods on Strings

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-/* Strings - a piece of text, or a sequence of characters 
-"&str" reference to a string, called a string reference.
-"String" dynamic, stored on the heap
-When obtaining string slices, must provide a range example: let pirate = "Bloodhound"; let first letter = &pirate[0..1];*/
+/* Format Macro - Similar to println!, but instead of printing a string, it returns a formatted string, inlcuding any interpolated contents
+Can then be assigned to a variable or set as a return value of a function*/
 
 
 fn main() {
-//Concatenation Example
-let mut full_name = String::from("Sylvester");
+let first_name = String::from("Sylvester");
 let last_name = "Stallone";
-full_name.push(' ');
-full_name.push_str(last_name);
-println!("{}", full_name);
 
-//Concatenation using the add method
-let first_name = String::from("Sylvester ");
-let last_name = String::from("Stallone");
+let icon = format!("{} {}", first_name, last_name);
+println!("{icon}"); //This does not take ownership and the variables can be re-used
+println!("{}", first_name);
 
-let full_name = first_name + &last_name;//must use & because + invokes a method on first_name and last_name becomes the argument. Ownership transfers from first_name to full_name
-println!("{}", full_name);
 
-//behind the scenes, this is what is being ran
-//fn add(mut self, other: &str) -> String {
-//self.push_str(other);
-//self
+//Common Methods on Strings
+
+let mut music_genres: &str = "    Rock, Metal, Country, Rap    ";
+println!("{}", music_genres.trim());//returns a string slice with leading and trailing white space removed
+println!("{}", music_genres.trim_start());//returns a string slice with the leading white space removed
+println!("{}", music_genres.trim_end()); //returns a string slice with the trailing white space removed
+
+music_genres = music_genres.trim();//trims leading and trailing white space and assigns to variable
+println!("{}", music_genres.to_uppercase());
+println!("{}", music_genres.to_lowercase());
+
+//replace() takes 2 arguments, what and what it is be replaced with
+println!("{}", music_genres.replace("a", "@"));//creates a new heap allocated string where every a is replaced with @
+
+//split allows us to split the string based on a specified delimiter
+let genres: Vec<&str> = music_genres.split(", ").collect();//", " is the specified delimiter that we are splitting on. If we want to collect all the pieces into a vector, we define the specific Vec<Type> and use the .collect() method on the split struct
+println!("{:?}", genres);//will output ["Rock", "Metal", "Country", "Rap"], split is helpful if you are splitting addresses based on commas, first name and last name separated by a space, etc.
 }
