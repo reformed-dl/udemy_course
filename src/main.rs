3630f8dbd2e38bba9fdb82fd68a52d6bab3442d1
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-13 15:57:09 -0400

    User Input
    Prompting and Collecting User Input with Strings.

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,18 @@
-/* Format Macro - Similar to println!, but instead of printing a string, it returns a formatted string, inlcuding any interpolated contents
-Can then be assigned to a variable or set as a return value of a function*/
-
+use std::io; //io - input/output data coming in and going out of our program
 
 fn main() {
-let first_name = String::from("Sylvester");
-let last_name = "Stallone";
+   //stdin() is a fn that returns a struct also called Stdin and that struct contains a method called read_line
+    let mut name = String::new();
+    println!("What is your name?");
+    io::stdin().read_line(&mut name); //buf parameter accepts a mutable reference to a String
+    //what happens here is when ran, output is What is your name?, then user inputs data in the terminal and we store it
+    println!("Hello {}", name);
 
-let icon = format!("{} {}", first_name, last_name);
-println!("{icon}"); //This does not take ownership and the variables can be re-used
-println!("{}", first_name);
-
-
-//Common Methods on Strings
-
-let mut music_genres: &str = "    Rock, Metal, Country, Rap    ";
-println!("{}", music_genres.trim());//returns a string slice with leading and trailing white space removed
-println!("{}", music_genres.trim_start());//returns a string slice with the leading white space removed
-println!("{}", music_genres.trim_end()); //returns a string slice with the trailing white space removed
-
-music_genres = music_genres.trim();//trims leading and trailing white space and assigns to variable
-println!("{}", music_genres.to_uppercase());
-println!("{}", music_genres.to_lowercase());
-
-//replace() takes 2 arguments, what and what it is be replaced with
-println!("{}", music_genres.replace("a", "@"));//creates a new heap allocated string where every a is replaced with @
-
-//split allows us to split the string based on a specified delimiter
-let genres: Vec<&str> = music_genres.split(", ").collect();//", " is the specified delimiter that we are splitting on. If we want to collect all the pieces into a vector, we define the specific Vec<Type> and use the .collect() method on the split struct
-println!("{:?}", genres);//will output ["Rock", "Metal", "Country", "Rap"], split is helpful if you are splitting addresses based on commas, first name and last name separated by a space, etc.
+    //since read_line is a Result Enum, a better way to handle both the Ok and Err possibilities is with a match statement
+    let mut whole_name = String::new();
+    println!("What is your name?");
+    match io::stdin().read_line(&mut whole_name) {
+        Ok(_) => println!("Hello {}", whole_name.trim()),//trim removes the extra line from the user hitting enter
+        Err(message) => println!("There was an error: {}", message),
+    }
 }
