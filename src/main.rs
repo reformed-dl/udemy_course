3fba8d175ff46ffe7ae0e3181dadb8025b079e95
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-14 12:59:54 -0400

    Strings Challenge
    Challenge and Solution set from String Section (15)

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,74 @@
-use std::io; //io - input/output data coming in and going out of our program
+/*
+Define a `make_money` function that accepts a mutable
+String reference. The function should concatenate
+the characters "$$$" to the end of the String.
+Invoke the function in `main`.
 
+Define a `trim_and_capitalize` function that accepts
+a string slice. It should return a String with
+all whitespace removed and all characters in uppercase.
+Invoke the function in `main`.
+
+Define an `elements` function that accepts a string
+slice. It should split the string by all occurrences
+of the `!` symbol and return a vector of the string
+slices. Invoke the function in `main`.
+
+Example:
+elements("Gold!Silver!Platinum")
+=> Vector of ["Gold", "Silver", "Platinum"]
+
+Define a `get_identity` function. The function should
+ask the user for their first and last name in TWO
+steps (i.e., collect user input twice). Make sure
+to communicate the instructions to the user.
+For each Result enum you receive, call the `expect`
+method and provide a custom error message (like
+"Failed to collect first name"). Return a String
+with the first and last names combined. Invoke
+the `get_identity` function in `main`, and output the
+returned String.
+
+Example:
 fn main() {
-   //stdin() is a fn that returns a struct also called Stdin and that struct contains a method called read_line
-    let mut name = String::new();
-    println!("What is your name?");
-    io::stdin().read_line(&mut name); //buf parameter accepts a mutable reference to a String
-    //what happens here is when ran, output is What is your name?, then user inputs data in the terminal and we store it
-    println!("Hello {}", name);
+  let name = get_identity();
+   println!("{name}"); // Bill Murray
+}*/
+use std::io;
 
-    //since read_line is a Result Enum, a better way to handle both the Ok and Err possibilities is with a match statement
-    let mut whole_name = String::new();
-    println!("What is your name?");
-    match io::stdin().read_line(&mut whole_name) {
-        Ok(_) => println!("Hello {}", whole_name.trim()),//trim removes the extra line from the user hitting enter
-        Err(message) => println!("There was an error: {}", message),
-    }
+fn make_money(text: &mut String) {
+    text.push_str("$$$");
+}
+fn trim_and_capitalize(text: &str) -> String {
+    text.trim().to_uppercase()
+    
+}
+fn elements(element: &str) -> Vec<&str> {
+    element.split("!").collect()//if return type was not provided, you could use a turbofish operator .collect::<Vec<&str>>()
+}
+fn get_identity() -> String {
+    let mut first_name = String::new();
+    let mut last_name = String::new();
+    let input = io::stdin();
+    println!("What is your first name?");
+    input.read_line(&mut first_name).expect("failed to collect first name");
+    println!("What is your last name?");
+    input.read_line(&mut last_name).expect("failed to collect last name");
+    format!("{} {}", first_name.trim(), last_name.trim())
+}
+fn main() {
+  let mut amount = String::from("1000");
+  make_money(&mut amount);
+  println!("{}", amount);
+
+  let name = trim_and_capitalize("    Reformed and Salty   ");
+  println!("{}", name);
+
+  let element = elements("Volcano!Pyramid!Spaceship");
+  println!("{:?}", element);
+
+  let input_name = get_identity();
+  println!("Nice to meet you {}", input_name);
+
+  println!("Welcome to the world of shadowy coders {}", get_identity());
 }
