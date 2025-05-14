b39614d125531460dda4c174d7e6af002cb133cd
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-14 17:01:48 -0400

    HashMaps Intro

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,26 @@
-/*
-Define a `make_money` function that accepts a mutable
-String reference. The function should concatenate
-the characters "$$$" to the end of the String.
-Invoke the function in `main`.
+/*Hash Maps - collection type that consists of key-value pairs
+Keys must be unique, Values can be duplicated
+Other languages -> dictionary, hash, associative array, map
+Order is not important, but focuses on connections(mappings) think a restaurant menu. Dish Name is the Key, Price is the Value. Order is not imporant, but the connections are.
+Stored on the heap, dynamic.
 
-Define a `trim_and_capitalize` function that accepts
-a string slice. It should return a String with
-all whitespace removed and all characters in uppercase.
-Invoke the function in `main`.
+found within the standard collections library - use std::collections::Hashmap;
 
-Define an `elements` function that accepts a string
-slice. It should split the string by all occurrences
-of the `!` symbol and return a vector of the string
-slices. Invoke the function in `main`.
+Hashmap::new(); -> New constructor function*/
 
-Example:
-elements("Gold!Silver!Platinum")
-=> Vector of ["Gold", "Silver", "Platinum"]
-
-Define a `get_identity` function. The function should
-ask the user for their first and last name in TWO
-steps (i.e., collect user input twice). Make sure
-to communicate the instructions to the user.
-For each Result enum you receive, call the `expect`
-method and provide a custom error message (like
-"Failed to collect first name"). Return a String
-with the first and last names combined. Invoke
-the `get_identity` function in `main`, and output the
-returned String.
-
-Example:
+use std::collections::HashMap;
 fn main() {
-  let name = get_identity();
-   println!("{name}"); // Bill Murray
-}*/
-use std::io;
+   let mut menu: HashMap<String, f64> = HashMap::new();
+    menu.insert(String::from("Steak"), 29.99);
+    menu.insert(String::from("Tuna"), 29.99);
+    menu.insert(String::from("Burger"), 13.99);
+    println!("{:?}", menu);
 
-fn make_money(text: &mut String) {
-    text.push_str("$$$");
-}
-fn trim_and_capitalize(text: &str) -> String {
-    text.trim().to_uppercase()
-    
-}
-fn elements(element: &str) -> Vec<&str> {
-    element.split("!").collect()//if return type was not provided, you could use a turbofish operator .collect::<Vec<&str>>()
-}
-fn get_identity() -> String {
-    let mut first_name = String::new();
-    let mut last_name = String::new();
-    let input = io::stdin();
-    println!("What is your first name?");
-    input.read_line(&mut first_name).expect("failed to collect first name");
-    println!("What is your last name?");
-    input.read_line(&mut last_name).expect("failed to collect last name");
-    format!("{} {}", first_name.trim(), last_name.trim())
-}
-fn main() {
-  let mut amount = String::from("1000");
-  make_money(&mut amount);
-  println!("{}", amount);
+    let mut city_capitals = HashMap::<&str, &str>::new(); //turbofish construction
+    city_capitals.insert("Tallahassee", "Florida");
+    city_capitals.insert("Albany", "New York");
+    city_capitals.insert("Annaheim", "California");
+    println!("{:?}", city_capitals);
 
-  let name = trim_and_capitalize("    Reformed and Salty   ");
-  println!("{}", name);
-
-  let element = elements("Volcano!Pyramid!Spaceship");
-  println!("{:?}", element);
-
-  let input_name = get_identity();
-  println!("Nice to meet you {}", input_name);
-
-  println!("Welcome to the world of shadowy coders {}", get_identity());
+    //Rust will also infer the Key and Value types once values are provided
 }
