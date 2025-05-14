d15d9373db4fd0309b32b568369a31f817547175
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-14 18:53:47 -0400

    HashMap-Ownership

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-/*HashMap::from() is a function that accepts an array of tuples -> [(K, V), (K, V), (K, V)]*/
+/*HashMaps are stored on the Heap and follow the same ownership rules as other Heap Types.
+The HashMap takes ownership of data assigned to it that do not implement the copy trait*/
 
 use std::collections::HashMap;
 fn main() {
-    let data: [(&str, i8); 3] = [("George", 7), ("Wanda", 4), ("Tommy", 12)];
-    let mut employee_data = HashMap::from(data);
-    println!("{:?}", employee_data);
+   let drink = String::from("Latte");
+   let milk = String::from("Oat Milk");
+   let mut coffee = HashMap::new();
+   coffee.insert(drink, milk);//ownership is transferred from the drink, milk variables to the HashMap
+   println!("{:?}", coffee);
+   println!("{}", coffee.len());//len() provides the number of Key-Value pairs contained within the HashMap (1)
 
-    //remove method returns and Option Enum Some()->there is associated data that will be extracted or None->No associated date
-    let tommy = employee_data.remove("Tommy");//Tommy is the key and the associated data being extracted is 12
-    println!("{:?}", tommy);//returns Some(12)
-    println!("{:?}", tommy.unwrap());//returns just 12 without Some(12)
-
-
-    println!("{:?}", employee_data);//The Key-Value pair of Tommy has now been removed from the HashMap
-
-    let carol = employee_data.remove("Carol");
-    println!("{:?}", carol);//Since there is no "Carol" Key, the Option Enum is None
+   //There are options to retain ownership
+   //We can pass in references to the Strings &String
+   //Or we can hardcode the HashMap type with &str and then pass in references to the Strings due to the dereferencing feature
+   let mut capitals: HashMap<&str, &str> = HashMap::new();
+   let city = String::from("Tallahassee");
+   let state = String::from("Florida");
+   capitals.insert(&city, &state);
+   capitals.insert("Albany", "New York");//this allows us the flexibility to pass in new string slices and not have type mismatches
+   println!("{:?}", capitals);
+   println!("{} {}", city, state);
 }
