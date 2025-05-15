2d675ea0c525efc0584ef284aa0a36bfaea6207e
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-15 18:47:45 -0400

    HashSets

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,34 @@
-use std::collections::HashMap;
+//HashSet - Distinct Type, but utilizes the HashMap behind the scenes to implement it's functionality. Often a caled a Set in other languages
+//A collection type that stores unique values. Solves the problem of duplication. Prohibits duplicate values
+
+use std::collections::HashSet;
 fn main() {
-  let mut coffe_pairings: HashMap<&str, &str> = HashMap::new();
-  let drink = String::from("Latte");
-  let milk = String::from("Oat Milk");
-  coffe_pairings.insert(&drink, &milk);
-  coffe_pairings.insert("Flat White", "Almond Milk");
-  println!("{:?}", coffe_pairings);
+  let mut concert_que: HashSet<&str> = HashSet::new();
 
-  //Every Key must be unique, if we have duplicate keys, the old value will be overwritten by the new value
-  coffe_pairings.insert("Latte", "Pistachio Milk");
-  println!("{:?}", coffe_pairings);//Previous Key - Value pair has been overwritten
+    concert_que.insert("Molly");
+    concert_que.insert("Megan");
+    println!("{:?}", concert_que);
+    println!("{:?}", concert_que.len());
 
-  //Entry Method - Accepts a HashMap Key and returns an Enum called Entry. Entry has 2 Variants, Occupied-the possibility that the given key exists and the
-  //Vacant - possibility that the given key does not Vacant variant and will insert the Key and associated Value
-  //.or_insert() will only come into play for the Vacant 
-  coffe_pairings.entry("Latte").or_insert("Skim Milk");
-  println!("{:?}", coffe_pairings);//nothing changes
-  coffe_pairings.entry("Americano").or_insert("Whole Milk");
-  println!("{:?}", coffe_pairings);//new Key-Value pair inserted
+    concert_que.insert("Molly");
+    println!("{:?}", concert_que);//Nothing is added, because it won't allow duplicates
+
+    //remove will remove the value and return a boolean
+    println!("{:?}", concert_que.remove("Megan"));
+    println!("{:?}", concert_que.remove("Megan"));//already removed so will return false
+
+    //Contains check to see if the argument is contained within the HashSet, returns a boolean
+    println!("{:?}", concert_que.contains("Megan"));
+    println!("{:?}", concert_que.contains("Molly"));
+
+    //Get returns an Option Enum - Some(&Associated Data) or None
+    println!("{:?}", concert_que.get("Molly"));//Some("Molly")
+    println!("{:?}", concert_que.get("Megan"));//None
+
+
+
+
+
+
 
 }
