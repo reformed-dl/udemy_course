9e0d910e645818725622ca26e883678b60cc132e
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-18 08:54:22 -0400

    Error Handling-Asking for User Input

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,55 @@
-/*Error Handling
-
-eprintln!() is a macro that prints messages to the standard error, println!() prints messages to the standard output. Think of standard output and
-standard error as different tv channels. We can chooses which channel to send different outputs to.
-
-To create a txt file from the terminal in VS Code, cargo run > filename.txt, anything in the compiler that prints to the standard output will
-print to the txt file, anything that prints to the error file will terminal */
-
-use std::fs::File; //we are accessing the standard library, fs is the file system module, File is a struct that models a file that we can read/write
-                   //This struct contains an Associated function called Open, which is a Result Enum
-                   //Success = Ok->associated data is the File struct;
-                   //Failure = Err -> associated data is error type that holds specific information about the error that occurred
+/*Error Handling-Asking the User for Input
+Instead of hard coding the file name, we will ask the user to input the file name they would like to read
+As covered previously, in order to collect input we use the the stdin fn from the input/output module (io)
+ */
+use std::fs::File;
+use std::io::stdin;
 use std::process;
 
 fn main() {
-    let file = File::open("story.txt"); //Since story.txt already exists within the same project, we don't have to provide the full path
-    println!("{:#?}", file); //Returns Ok with the entire File Struct which stores fields about the fie: path, read: bool, write: bool, etc
+  //start by giving the user instructions
+  println!("Please enter the name of the file you would like to read");
+  let mut input = String::new();
 
-    let nonsense = File::open("nonsense.txt");
-    println!("{:#?}", nonsense); //Returns Err Os Struct { code: 2, kind: NotFound, message: "No such file or directory", }
+  //when stdin() function in invoked, it will return a Stdin struct that contains the read_line() method, which returns a Result Enum
+  //user input is stored in "input" variable
+  //Using a match statement here works, but the Ok variant is essentially purposeless
+  /* match stdin().read_line(&mut input) {
+    Ok(size) => println!("The size of the user entry in bytes is {}", size), //Ok variant doesn't stor the user entry, just the size in bytes
+    Err(error) => {
+      eprintln!("Something went wrong collecting user input. The erorr was {:?}", error);
+      process::exit(1)
+    }
+  } */
 
-    //Using a match statement to account for both possibilities
-    let next_file = match File::open("story.txt") {
-      Ok(next_file) => next_file,//Ok arm is matched and this is returned and assigned to the next_file variable
-      Err(error) => {
-        eprintln!("Something went wrong reading the file. The error was {:?}", error);
-        process::exit(1)//since we matched on the Ok arm, no termination and code continues
-      }
-    };
-    println!("{:#?}", next_file);//File Struct is printed   
+  //alternative solution is utilizing the if let syntax. This allows us to cover a specific Enum Variant, only one.
+  //we only want to run this code if we in fact have an error, if not, we want to proceed with the remainder of the code
 
-    let new_file = match File::open("nofile.txt") {
-        Ok(new_file) => new_file,
-        Err(error) => {
-            eprintln!("Something went wrong reading the file. The error was {:?}", error); //error message will be sent to the error output
-            process::exit(1) //program will terminate
-        }
-    };
-    println!("{:#?}", new_file);//this code is never ran
+  let user_requested_file = stdin().read_line(&mut input);
+  if let Err(error) = user_requested_file {
+    eprintln!("Something went wrong collecting user input. The erorr was {:?}", error);
+    process::exit(1)
+  }
+  
+  //Now we can run a match statement
+  let file = match File::open(&input.trim()) {//trim() is necessary because when user hits enter, the \n linebreak is captured
+    Ok(file) => file,
+    Err(error) => {
+      eprintln!("Something went wrong reading the file. The error was {}", error);
+      process::exit(1)
+    }
+  };
+  println!("{:#?}", file);
 
+/*Walking through the logic of this code
+println! requests user input
+mut input = String::new() provides a variable where user input will be stored
+user is prompted to enter file they want to read
+if there was an error in collecting user input, Err would be assigned to "user_requested_file" the "if let" syntax would run, error message output, program terminates
+if we are able to successfully caputure user input, input will be assigned to "user_requested_file" and "if let" syntax would NOT run
+code progresses, we trim the content of what was assigned to "input" and match content and assign results back to "file" variable
+If file can be found, matched to Ok arm, file is extracted and assigned back to "file" variable
+file variable contents is output 
+If file cannot be found, Err arm is matched, error code is output, program is terminated
+ */
 }
