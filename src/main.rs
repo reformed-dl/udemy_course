de904ebcb67c5443399da24137f830ec20ec8f81
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-05-21 06:14:19 -0400

    Error Handling-read_to_string associated function
    utilizing the read_to_string associated function on the file system(fs) module to replace File::open and read_to_string() methods

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-/*Error Handling- Further refactor
+/*Error Handling- read_to_string associated function.
+This is different from the read_to_string() method that is called on the File struct
+the file system module "fs", includes a helpful function called read_to_string that accomplishes everything we want and wrote code for previously
+the read_to_string function accepts a file path, attempts to open the file, reads the contents of the file to a heap string and then return the string
+packaged up in the Result Enums Ok Variant. If something fails along the way, it instead returns the io:Error type packaged up in the Result Enums Err Variant
 */
 
-use std::fs::File;
-use std::io::{self, stdin, Read};
+//use std::fs::File; we are going to remove File and stop at fs
+use std::fs;
+//use std::io::{self, stdin, Read}; we no longer need the Read trait either
+use std::io::{self, stdin};
 
 
 fn main() {
@@ -10,7 +16,7 @@ fn main() {
    let file_result = read_file();
    
    match file_result {
-    Ok(conents) => println!("{}", conents),
+    Ok(contents) => println!("{}", contents),
     Err(error) => {
       eprintln!("There was an error: {}", error)
     }
@@ -19,20 +25,17 @@ fn main() {
 
 fn read_file() -> Result<String, io::Error> {
   println!("Please enter the name of the file you would like to read");
-  
-    let mut input = String::new();
 
+    let mut input = String::new();
     stdin().read_line(&mut input)?;
 
-    let mut file_contents = String::new();
-  
-    File::open(&mut input.trim())?.read_to_string(&mut file_contents)?;
-   // The only reason why we needed the file variable is so we could call read_to_string() on the file
-   // both open and read_to_string return a Result type
-   // the ? will either return early or produce the actual File struct, it is that File struct that has the read_to_string() method, so I can add that on
-   // File::open(&mut input.trim())?.read_to_string(&mut file_contents)?;
+    fs::read_to_string(input.trim())//no semicolon for the implicit return
 
-    Ok(file_contents)
+    //the read_to_function associated function basically does everything coded below
+    //let mut file_contents = String::new();
+    //File::open(&mut input.trim())?.read_to_string(&mut file_contents)?;
+   
+    //Ok(file_contents)
 }
   
  
