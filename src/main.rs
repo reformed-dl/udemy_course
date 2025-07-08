f66e52e053a6c127c4f178bc3739d6d723f969ce
reformed-dl <197158047+reformed-dl@users.noreply.github.com>
2025-07-08 16:08:58 -0400

    Trait Bounds to Conditionally Implement Methods

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-/* Traits as Function Return Types
-    We can set the Return Type of a Function to be any Type that Implements a specific Trait or Traits
+/* Trait Bounds to Conditionally Implement Methods
+    *Trait bounds are when we attach one or more Traits to a Generic. It enforces constraints on what that possible Type could be. Any Type must implement the 
+    given Traits. It introduces limitations/boundaries.
 */
 
 use std::collections::HashMap;
+use std::fmt::Display; // must add this use import to add the trait bound for Display in the impl block below
 
 trait Accommodation {
     fn book(&mut self, name: &str, nights: u32) -> ();
@@ -14,32 +16,38 @@ trait Description {
     }
 }
 
+// Expanding Hotel Struct to accept a Generic Type T, once we make this change, all of the existing impl blocks will show an error as they expect a defined type.
 #[derive(Debug)]
-struct Hotel {
-    name: String,
+struct Hotel<T> {
+    name: T,
     reservation: HashMap<String, u32>,
 }
-
-impl Hotel {
-    fn new(name: &str) -> Self {
+// In order to have this code work, we have to use a special syntax that lets Rust know that we want these methods to work on any generic T
+// We will also add the trait bound here, so that Rust knows whatever type T is, it must implement the Display Trait
+// We will split up these impl blocks so we have more flexibility on the different types we can use and not limit our constructor function
+impl<T> Hotel<T> {
+    fn new(name: T) -> Self {// we have to change the parameter type her from &str to the generic T to have the flexibility that we want
         Self {
-            name: name.to_string(),
+            name, // use shorthand syntax here to assign whatever is provided in the name parameter to the name field
             reservation: HashMap::new(),
         }
     }
+}
 
+impl<T: Display> Hotel<T> {
     fn summarize(&self) -> String {
-        format!("{}: {}", self.name, self.get_description())
+        format!("{}: {}", self.name, self.get_description()) // because T is a generic, Rust cannot guarantee that it will implement the display {} trait
+                                                            // We can solve this problem with an trait bound, an additional constraint on the generic type
     }
 }
 
-impl Accommodation for Hotel {
+impl<T> Accommodation for Hotel<T> {
     fn book(&mut self, name: &str, nights: u32) -> () {
         self.reservation.insert(name.to_string(), nights); 
     }
 }
 
-impl Description for Hotel {} 
+impl<T> Description for Hotel<T> {} 
 
 #[derive(Debug)]
 struct AirBnB {
@@ -73,10 +81,6 @@ fn book_for_one_night(entity: &mut (impl Accommodation + Description), guest: &s
     entity.book(guest, 1);
 }
 
-// Showing both the Trait Bound Syntax and the Where Clause Syntax
-//Trait Bound:
-// fn mix_and_match<T: Accommodation + Description, U: Accommodation>(first: &mut T, second: &mut U, guest: &str) {code body}
-// Where Cluase accomplishes the same thing, but with a different syntax
 fn mix_and_match<T, U>(first: &mut T, second: &mut U, guest: &str)  
 where 
     T: Accommodation + Description,
@@ -87,18 +91,18 @@ where
     second.book(guest, 2);
 }
 
-// Traits as a Function Return Type
 fn choose_best_place_to_stay() -> impl Accommodation + Description {
     Hotel::new("The Luxe")
-    //Although the return type is any type that implements both of these traits, we can not have more than one type or we will get an error.
 }
 
-
 fn main() {
-    let mut hotel = choose_best_place_to_stay();// Analyzer identifies the type as any type that implements the Accommodation Trait, not Hotel Type
-    let mut airbnb = AirBnB::new("Peter");   
-    mix_and_match(&mut hotel, &mut airbnb, "Penelope");
-    //initially mix_and_match would not compile b/c the parameter type requies a type that implements Accommodation and Description Traits, had to change return type
-    //of choose_best_place_to_stay to be a type that implements both Accommodation and Description in order to compile
+    let hotel1 = Hotel::new(String::from("The Luxe"));
+    println!("{}", hotel1.summarize()); // Can call this method because the generic T in this case implements the Display Trait
 
+    let hotel2 = Hotel::new("The Golden Standard");
+    println!("{}", hotel2.summarize()); // String slice will also support the summarize method
+
+    let hotel3 = Hotel::new(vec!["The Sweet Escape", " The Hills"]);
+    //println!("{}", hotel3.summarize()); // This will not compile because the type for T is a vec and does not implement the Display trait
+    println!("{:?}", hotel3);
 }
